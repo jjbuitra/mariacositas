af71df61fddae199f5a1d46230c9bbfb7a841b79
jjbuitra <94945837+jjbuitra@users.noreply.github.com>
2023-01-18 18:22:34 -0500

    proyecto repaso

---
+++ maria_cositas/maria_cositas/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace maria_cositas.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
